7cab3213b898a9256dd20cc9644639ad3052cedc
meteora277 <justin.s.diaz@gmail.com>
2021-12-03 16:13:00 -0500

    created withdrawal subclass, and transaction Superclass

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-let balance = 500.00;
+class Transaction {
+  constructor(amount, account) {
 
-class Withdrawal {
-
-  constructor(amount) {
     this.amount = amount;
+    this.account = account;
   }
+}
+class Withdrawal extends Transaction {
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
+class Deposit extends Transaction {
 
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+console.log(myAccount)
+
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+let t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
